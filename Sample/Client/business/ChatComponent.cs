ed9f7957ceb89c631c8381c4fb019843decce8ea
lizs <lizs4ever@163.com>
2016-03-05 15:21:51 +0800

    MIT license

--- Sample/Client/business/ChatComponent.cs
+++ Sample/Client/business/ChatComponent.cs
@@ -1,4 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿#region MIT
+//  /*The MIT License (MIT)
+// 
+//  Copyright 2016 lizs lizs4ever@163.com
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//   * */
+#endregion
+using System.Threading.Tasks;
 using Shared;
 using socket4net;
 
@@ -10,32 +34,26 @@ namespace Sample
         ///     请求创建一个Ship
         /// </summary>
         /// <returns></returns>
-        public async Task<RpcResult> Add()
+        public void Add()
         {
-            var ret =
-                await
-                    RequestAsync(0, GetAncestor<Player>().Id, (short)EOps.Create, null, 0, Id);
-
-            if (!ret)
-                Logger.Ins.Error("Add failed!");
-
-            return ret;
+            RequestAsync(0, GetAncestor<Player>().Id, (short)EOps.Create, null, 0, Id, (b, bytes) =>
+            {
+                if (!b)
+                    Logger.Ins.Error("Add failed!");
+            });
         }
 
         /// <summary>
         ///     请求删除所有Ship
         /// </summary>
         /// <returns></returns>
-        public async Task<RpcResult> Del()
+        public void Del()
         {
-            var ret =
-                await
-                    RequestAsync(0, GetAncestor<Player>().Id, (short)EOps.Destroy, null, 0, Id);
-
-            if (!ret)
-                Logger.Ins.Error("Add failed!");
-
-            return ret;
+            RequestAsync(0, GetAncestor<Player>().Id, (short)EOps.Destroy, null, 0, Id, (b, bytes) =>
+            {
+                if (!b)
+                    Logger.Ins.Error("Add failed!");
+            });
         } 
 
         /// <summary>
@@ -43,22 +61,19 @@ namespace Sample
         /// </summary>
         /// <param name="msg"></param>
         /// <returns></returns>
-        public async Task<bool> Echo(string msg)
+        public void Echo(string msg)
         {
-            var ret =
-                await
-                    RequestAsync(0, GetAncestor<Player>().Id, (short) EOps.Echo, new EchoProto {Message = msg},
-                        0, Id);
-
-            if (ret)
-            {
-                var proto = PiSerializer.Deserialize<EchoProto>(ret.Value);
-                Logger.Ins.Info(proto.Message);
-            }
-            else
-                Logger.Ins.Error("Echo failed!");
-
-            return ret;
+            RequestAsync(0, GetAncestor<Player>().Id, (short) EOps.Echo, new EchoProto {Message = msg},
+                0, Id, (b, bytes) =>
+                {
+                    if (b)
+                    {
+                        var proto = PiSerializer.Deserialize<EchoProto>(bytes);
+                        Logger.Ins.Info(proto.Message);
+                    }
+                    else
+                        Logger.Ins.Error("Echo failed!");
+                });
         }
 
         /// <summary>
