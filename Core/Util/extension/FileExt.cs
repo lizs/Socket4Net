53d1b778ee0ca7a190c7a35e005ce8a3598cda8c
lizs <lizs4ever@163.com>
2016-07-05 18:11:45 +0800

    generic stack

---
+++ Core/Util/extension/FileExt.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace socket4net
+{
+    /// <summary>
+    ///     extension for file system
+    /// </summary>
+    public static class FileSys
+    {
+        public static IEnumerable<string> EnumerateFiles(
+            string dir, string searchPattern, SearchOption searchOption = SearchOption.AllDirectories)
+        {
+#if NET45
+            return Directory.EnumerateFiles(dir, searchPattern, searchOption);
+#else
+            return Directory.GetFiles(dir, searchPattern, searchOption);
+#endif
+        }
+
+        public static IEnumerable<string> EnumerateDirectories(
+            string dir, string searchPattern, SearchOption searchOption = SearchOption.AllDirectories)
+        {
+#if NET45
+            return Directory.EnumerateDirectories(dir, searchPattern, searchOption);
+#else
+            return Directory.GetDirectories(dir, searchPattern, searchOption);
+#endif
+        }
+
+        public static IEnumerable<string> EnumerateFilesAndDirectories(
+            string dir, string searchPattern, SearchOption searchOption = SearchOption.AllDirectories)
+        {
+#if NET45
+            return Directory.EnumerateFileSystemEntries(dir, searchPattern, searchOption);
+#else
+            return EnumerateDirectories(dir, searchPattern, searchOption).Concat(EnumerateFiles(dir, searchPattern, searchOption));
+#endif
+        }
+
+        public static void Copy(string source, string dest, bool overwrite)
+        {
+            File.Copy(source, dest, overwrite);
+        }
+
+        public static void CopyDirectory(string source, string dest, bool overwrite)
+        {
+            if (!Directory.Exists(dest))
+            {
+                Directory.CreateDirectory(dest);
+            }
+
+            foreach (var file in Directory.GetFiles(source))
+            {
+                File.Copy(file, Path.Combine(dest, Path.GetFileName(file)), overwrite);
+            }
+
+            foreach (var directory in Directory.GetDirectories(source))
+            {
+                CopyDirectory(directory, Path.Combine(dest, Path.GetFileName(directory)), overwrite);
+            }
+        }
+
+        public static bool DirectoryExists(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public static void CreateDirectory(string path, bool hidden)
+        {
+            var directory = Directory.CreateDirectory(path);
+
+            if (hidden)
+            {
+                directory.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            }
+        }
+
+        public static void DeleteDirectory(string path)
+        {
+            Directory.Delete(path, true);
+        }
+
+        public static string ReadFile(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public static string[] ReadFileLines(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+
+        public static bool IsPathRooted(string path)
+        {
+            return Path.IsPathRooted(path);
+        }
+
+        public static string CurrentDirectory
+        {
+            get { return System.Environment.CurrentDirectory; }
+            set { System.Environment.CurrentDirectory = value; }
+        }
+
+        public static string NewLine
+        {
+            get { return System.Environment.NewLine; }
+        }
+
+        public static DateTime GetLastWriteTime(string file)
+        {
+            return File.GetLastWriteTime(file);
+        }
+
+        public static void Move(string source, string dest)
+        {
+            File.Move(source, dest);
+        }
+
+        public static void MoveDirectory(string source, string dest)
+        {
+            Directory.Move(source, dest);
+        }
+
+        public static bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public static void FileDelete(string path)
+        {
+            File.Delete(path);
+        }
+
+        public static IEnumerable<string> SplitLines(string value)
+        {
+            return value.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+        }
+
+        public static void WriteToFile(string path, string text)
+        {
+            File.WriteAllText(path, text);
+        }
+
+        public static Stream CreateFileStream(string filePath, FileMode mode)
+        {
+            return new FileStream(filePath, mode);
+        }
+
+        public static void WriteAllBytes(string filePath, byte[] bytes)
+        {
+            File.WriteAllBytes(filePath, bytes);
+        }
+
+        public static string GetWorkingDirectory(string path)
+        {
+            if (path.IsNullOrWhiteSpace())
+            {
+                return CurrentDirectory;
+            }
+
+            var realPath = GetFullPath(path);
+
+            if (FileExists(realPath) || DirectoryExists(realPath))
+            {
+                if ((File.GetAttributes(realPath) & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    return realPath;
+                }
+
+                return Path.GetDirectoryName(realPath);
+            }
+
+            return Path.GetDirectoryName(realPath);
+        }
+
+        public static string GetFullPath(string path)
+        {
+            return Path.GetFullPath(path);
+        }
+
+        public static string GetRelativeWorkingDirectory(string path)
+        {
+            if (path.IsNullOrWhiteSpace())
+            {
+                return path;
+            }
+
+            if (FileExists(path) || DirectoryExists(path))
+            {
+                if ((File.GetAttributes(path) & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    return path;
+                }
+
+                return Path.GetDirectoryName(path);
+            }
+
+            return Path.GetDirectoryName(path);
+        }
+
+        public static string[] GetNodes(string path)
+        {
+            var dir = GetRelativeWorkingDirectory(path);
+            return dir.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string WorkingDirectory
+        {
+            get { return AppDomain.CurrentDomain.BaseDirectory; }
+        }
+    }
+}
+
+
---
+++ Core/Util/struct/Stack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace socket4net
+{
+#if NET35
+    /// <summary>
+    ///     stack
+    ///     implemented by List
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T>
+    {
+        private readonly List<T> _inner;
+
+        public Stack()
+        {
+            _inner = new List<T>();
+        }
+
+        public Stack(IEnumerable<T> collection)
+        {
+            _inner = new List<T>(collection);
+        }
+
+        public Stack(int capacity)
+        {
+            _inner = new List<T>(capacity);
+        }
+        public int Count { get { return _inner.Count; } }
+
+        public void Clear()
+        {
+             _inner.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return  _inner.Contains(item);
+        }
+
+        public T Peek()
+        {
+            return _inner.LastOrDefault();
+        }
+
+        public T Pop()
+        {
+            if(_inner.IsNullOrEmpty())
+                throw new Exception("Stack is empty!");
+
+            var ret = _inner.Last();
+            _inner.RemoveAt(_inner.Count - 1);
+            return ret;
+        }
+
+        public void Push(T item)
+        {
+            _inner.Add(item);
+        }
+    }
+#endif
+}
